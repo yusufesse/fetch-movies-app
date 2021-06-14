441756356d1e248dc205e2587133cb0150425fae
yusufesse <63024445+yusufesse@users.noreply.github.com>
2021-06-14 15:31:04 +0100

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -19,4 +19,4 @@ const MovieList = (props) => {
 };
 
 export default MovieList;
-//loop through movies array of obj from api, where for every current object, render a movie component (html structure) and pass the propert names for each object. The movie component instance will render the html structure (jsx) of movie component. And it will pass these props data for it to be dispplayed to DOM.
+
